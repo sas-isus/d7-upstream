2c799c3d3e90b7770d976f4d55645e294871cf4c
Gaylord Holder <holder@sas.upenn.edu>
2018-05-22 21:13:17 -0400

    Cleaned up code layout for config.php and removed unnecessary comments.
    Added comments to set $drupal_hash_salt correctly in default.settings.redirect-site.php

--- sites/default/default.settings.redirects-site.php
+++ sites/default/default.settings.redirects-site.php
@@ -4,6 +4,9 @@
 //global $primary_domain;
 //$primary_domain = 'www.UPDATEME.upenn.edu';
 
+//global $drupal_hash_salt;
+//$drupal_hash_salt = 'abc';
+
 // RewriteMap example
 /*
  * global $RewriteMap;
