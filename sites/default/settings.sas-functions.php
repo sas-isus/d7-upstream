7eb0b7b1abbeffbd16af1c3d742e74ee2085c5e0
Gaylord Holder <holder@sas.upenn.edu>
2018-12-28 08:34:26 -0500

    Updated php code to correctly handle proxies and subsites.
    All site specific data needed for redirects, shib, drupal_hash_salt, and local
    redirects are now in site.php.
    
    settings.sas-functions.php has all the helper logic
    
    settings.sas.php has the basic logic about when to issue redirects from
    + http  to https
    + non-canonical urls to preferred urls
    + old internal urls to newer urls
    
    settings.sas.php also sets the simplesaml directory and the drupal_hash_salt variable.
    
    This code has been tested and put in production on:
    	www.bio.upenn.edu
    	www.sas.upenn.edu/outcomes
    	katz.sas.upenn.edu
    	nais.sas.upenn.edu
    
    It is in dev on (modify etc hosts so www.sas.upenn.edu points to dev-proxy1.sas.upenn.edu):
    	www.sas.upenn.edu/rewards-and-recognition
    	www.sas.upenn.edu/strategic-plan
    	www.sas.upenn.edu/bbb
    	sas-bc
    	sas-eacl
    	sas-physics
    
    No problems have been found in the last month and I consider the code stable.

---
+++ sites/default/settings.sas-functions.php
@@ -0,0 +1,76 @@
+<?php
+
+if (file_exists(__DIR__ . '/site.php')) {
+    include __DIR__ . "/site.php";
+}
+
+
+function isPantheonSite () {
+	return(preg_match('@pantheonsite.io@',$_SERVER['HTTP_HOST']));
+}
+
+function isD8() {
+	return(isset($_ENV['HOME']) && (file_exists($_ENV['HOME'].'/code/web') || file_exists($_ENV['HOME'].'/code/modules/contrib')));
+}
+
+function isProxied() {
+	// HTTP_X_FORWARDED_HOST is undefined when coming in through the load-balancer
+	// HTTP_HOST == HTTP_X_FORWARDED_HOST for direct access to pantheon + via CDN
+	// via proxy, HTTP_HOST -> 'live-sas-school.pantheonsite.io' while
+	//   HTTP_X_FORWARDED_HOST -> 'www.sas.upenn.edu, live-sas-school.pantheonsite.io'
+	return(	   isset($_SERVER['HTTP_X_FORWARDED_HOST']) 
+        	&& ($_SERVER['HTTP_HOST'] != $_SERVER['HTTP_X_FORWARDED_HOST']));
+}
+
+function isHTTP() {
+    // From https://pantheon.io/docs/domains/ 
+    return (   !isset($_SERVER['HTTP_USER_AGENT_HTTPS']) 
+            || $_SERVER['HTTP_USER_AGENT_HTTPS'] != 'ON'
+            || empty($_SERVER['HTTPS']) 
+            || $_SERVER['HTTPS'] == "OFF");
+} 
+
+function getCanonicalHost() {
+	global $CanonicalHost;
+
+	return($CanonicalHost);
+}
+
+function getBaseURL() {
+	global $BaseURL;
+
+	$URL = 'https://' . $_SERVER['HTTP_HOST'];
+	if (isProxied() && isset($BaseURL) ) {
+		$URL = $BaseURL;
+	}
+
+	return($URL);
+}
+
+function getEntityID() {
+	global $EntityID;
+
+	$ID = 'https://' . $_SERVER['HTTP_HOST'];
+	if (isset($EntityID)) {
+		$ID = $EntityID;
+	}
+
+	return($ID);
+}
+
+function redirectTo($url,$cache = True) {
+	if (extension_loaded('newrelic')) {
+		newrelic_name_transaction("redirect");
+	}
+
+	// From https://github.com/pantheon-systems/documentation/issues/1280
+	if (!$cache) {
+		header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+		header("Cache-Control: post-check=0, pre-check=0", false);
+		header("Pragma: no-cache");
+	}
+            
+	header('HTTP/1.0 301 Moved Permanently');
+	header('Location: '. $url);
+	exit();
+}
